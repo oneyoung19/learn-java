49bc9d1ded8fe89ef57fe82e4303078e2d198a72
Andy Yang <2878847069@qq.com>
2025-04-09 13:42:27 +0800

    feat(my-app): 添加 JSON 序列化功能并优化项目配置
    - 在项目中引入 fastjson2 和 gson 依赖
    - 添加 maven-dependency-plugin 以自动化复制依赖到 lib 目录
    - 更新 .gitignore 文件，忽略 .DS_Store 和 lib 目录
    - 新增 README.md 文件，提供项目运行指南
    - 在 App.java 中添加 getUser 方法，返回 JSON 格式的用户信息

---
+++ README.md
@@ -0,0 +1,11 @@
+# Java
+
+```shell
+# 获取 classpath
+CLASS_PATH=$(mvn dependency:build-classpath | grep -v "\[INFO\]")
+
+# 运行
+java -cp "target/my-app-1.0.0.jar:lib/*:$CLASS_PATH" com.example.oneyoung.App
+
+java -cp "target/classes:$CLASS_PATH" com.example.oneyoung.App
+```
--- my-app/src/main/java/com/example/oneyoung/App.java
+++ my-app/src/main/java/com/example/oneyoung/App.java
@@ -1,10 +1,26 @@
 package com.example.oneyoung;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(getUser());
+    }
+    
+    public static String getUser() {
+        // 创建 Gson 实例
+        Gson gson = new Gson();
+        
+        // 创建 JSON 对象
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("name", "oneyoung");
+        
+        // 将 JSON 对象转换为字符串
+        return gson.toJson(jsonObject);
     }
 }
