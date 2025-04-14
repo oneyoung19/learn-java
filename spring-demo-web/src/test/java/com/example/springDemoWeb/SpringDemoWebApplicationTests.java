821eb29a6b727af64a28ac4999d3524ff96e9356
Andy Yang <2878847069@qq.com>
2025-04-14 19:35:43 +0800

    feat(spring-demo-web): 添加 Spring Boot 项目结构和基本配置

---
+++ spring-demo-web/src/test/java/com/example/springDemoWeb/SpringDemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDemoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
